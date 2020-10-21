8dc6421e174193ba72e52a9e7bc685af8c98bd12
Kristijan Kos <kkos1@tvz.hr>
2020-10-21 22:04:16 +0200

    Add config

---
+++ NoHotbarPickupConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+#pragma warning disable 649
+
+namespace NoHotbarPickup
+{
+	internal class NoHotbarPickupConfig : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ClientSide;
+
+		[Label("Items go into hotbar when inventory is full")]
+		[Tooltip("If disabled and inventory full, items can't be picked up, even if hotbar has room for them")]
+		[DefaultValue(true)]
+		public bool hotbarWhenFull;
+
+		[Label("Items should go into the last available slot of the inventory")]
+		[Tooltip("If off, items fill inventory from beginning")]
+		[DefaultValue(true)]
+		public bool pickupDirection;
+	}
+}
